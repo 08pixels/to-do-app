87bb415ff199c9911d82ffdbf0e37afbabe237af
08pixels <luizrodrigomarques@gmail.com>
2019-07-11 00:51:53 -0300

    acrescentando local storage (corrigir)

--- version #2/todo.js
+++ version #2/todo.js
@@ -4,7 +4,7 @@ var buttonElement= document.querySelector('#send');
 
 var doneElement = document.querySelector('#done');
 
-var todoList = new Map();
+var todoList = new Map(JSON.parse(localStorage.getItem('todoList'))) || new Map();
 
 function removeDoneAfter() {
     var elements = appElement.querySelectorAll('input');
@@ -16,6 +16,8 @@ function removeDoneAfter() {
             todoList.delete(element.parentElement);
         }
     }
+    
+    saveToStorage();
 }
 
 function renderTodo() {
@@ -38,20 +40,53 @@ function renderTodo() {
     newTodo.appendChild(newLabel);
     appElement.appendChild(newTodo);
     
-    todoList.set(newTodo, newText);
+    todoList.set(newTodo, inputElement.value);
     inputElement.value = '';
+    saveToStorage();
 }
 
-buttonElement.onclick = function() {
-    if(inputElement.value === "")
-        return;
-
-    renderTodo();
+function saveToStorage() {
+    localStorage.setItem('todoList', JSON.stringify([...todoList]));
 }
 
-onkeypress = function() {
+function enterEvent() {
     if(event.keyCode === 13)
         buttonElement.onclick();
 }
 
+function send() {
+    if(inputElement.value == "")
+        return;
+    
+    renderTodo();
+}
+
+function start() {
+
+    for(var [ref, text] of todoList) {
+        var newTodo = document.createElement('p');
+        var inputElement = document.createElement('input');
+        var labelElement = document.createElement('label');
+        var textElement = document.createTextNode(text);
+        
+        inputElement.setAttribute('type', 'checkbox');
+
+        inputElement.oninput = function() {
+            if(doneElement.checked && inputElement.checked) {
+                todoList.delete(newTodo);
+                appElement.removeChild(newTodo);
+            }
+        }
+
+        labelElement.appendChild(textElement);
+        newTodo.appendChild(inputElement);
+        newTodo.appendChild(labelElement);
+        appElement.appendChild(newTodo);
+    }
+}
+
+onkeypress = enterEvent;
+buttonElement.onclick = send;
 doneElement.oninput = removeDoneAfter;
+
+start();
